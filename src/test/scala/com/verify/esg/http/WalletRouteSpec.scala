a0be9e4efeeba6d4cbea6a788578dd87c33f32da
Devin Joubert <dvnjoubert0@gmail.com>
2023-02-14 11:46:08 +0200

    Add WalletRouteSpec

---
+++ src/test/scala/com/verify/esg/http/WalletRouteSpec.scala
@@ -0,0 +1,11 @@
+package com.verify.esg.http
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class WalletRouteSpec extends AnyFlatSpec {
+  behavior of "GET /wallet/{walletId}/friend"
+
+  it should "exist and be merry" in {
+    assert(1 === 1)
+  }
+}
