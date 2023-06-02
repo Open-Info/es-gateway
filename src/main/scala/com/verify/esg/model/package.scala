db290bd1832d80b1cdbe4037397e4ef74148ba0b
Devin Joubert <dvnjoubert0@gmail.com>
2023-06-02 10:44:37 +0200

    Remove Neo integrations from the service

--- src/main/scala/com/verify/esg/model/package.scala
+++ src/main/scala/com/verify/esg/model/package.scala
@@ -1,7 +1,6 @@
 package com.verify.esg
 
 import cats.syntax.all._
-import com.verify.esg.neo.DerivedNeoMappers
 import monix.newtypes.integrations.DerivedCirceCodec
 import monix.newtypes.{BuildFailure, NewtypeValidated, NewtypeWrapped}
 
@@ -9,7 +8,7 @@ import scala.util.matching.Regex
 
 package object model {
   type EthAddressId = EthAddressId.Type
-  object EthAddressId extends NewtypeValidated[String] with DerivedCirceCodec with DerivedNeoMappers {
+  object EthAddressId extends NewtypeValidated[String] with DerivedCirceCodec {
     private val regex: Regex = "^0x[0-9a-fA-F]{40}$".r
 
     override def apply(value: String): Either[BuildFailure[Type], Type] =
@@ -18,13 +17,13 @@ package object model {
         .as(unsafe(value.toLowerCase))
         .toRight(BuildFailure(s"'$value'"))
 
-    implicit class EthAddressOps(val ethString: String) extends AnyVal {
-      def unsafeEth: EthAddressId = unsafe(ethString)
+    implicit class EthAddressOps(val s: String) extends AnyVal {
+      def unsafeEth: EthAddressId = unsafe(s)
     }
 
     def fromString(ethString: String): Option[EthAddressId] = regex.findFirstIn(ethString).flatMap(apply(_).toOption)
   }
 
   type TransactionValue = TransactionValue.Type
-  object TransactionValue extends NewtypeWrapped[String] with DerivedCirceCodec with DerivedNeoMappers
+  object TransactionValue extends NewtypeWrapped[String] with DerivedCirceCodec
 }
--- src/main/scala/com/verify/esg/service/EsService.scala
+++ src/main/scala/com/verify/esg/service/EsService.scala
@@ -5,7 +5,6 @@ import cats.syntax.all._
 import com.verify.esg.EsServiceConfig
 import com.verify.esg.client.etherscan.EsClient
 import com.verify.esg.model.{EthAddressId, EthTransaction}
-import com.verify.esg.neo.TransactionNeo
 
 trait EsService[F[_]] {
   def getFriends(walletId: EthAddressId): F[Set[EthAddressId]]
@@ -15,31 +14,27 @@ trait EsService[F[_]] {
 object EsService {
   def apply[F[_]: Sync: Clock](
     esClient: EsClient[F],
-    transactionNeo: TransactionNeo[F],
     config: EsServiceConfig
   ): EsService[F] =
-    new EsServiceImpl[F](esClient, transactionNeo, config)
+    new EsServiceImpl[F](esClient, config)
 
   private final class EsServiceImpl[F[_]: Sync: Clock](
     esClient: EsClient[F],
-    transactionNeo: TransactionNeo[F],
     config: EsServiceConfig
   ) extends EsService[F] {
 
     override def getFriends(walletId: EthAddressId): F[Set[EthAddressId]] =
-      transactionsAndStore(walletId).map { transactions =>
+      transactions(walletId).map { transactions =>
         val wallets = transactions.flatMap(t => Vector(t.to.addressId, t.from.addressId))
         wallets - walletId
       }
 
     override def getTransactions(walletId: EthAddressId): F[Set[EthTransaction]] =
-      transactionsAndStore(walletId)
+      transactions(walletId)
 
-    private def transactionsAndStore(walletId: EthAddressId): F[Set[EthTransaction]] =
-      for {
-        esTransactions  <- esClient.getTransactions(walletId, config.numBlocks)
-        ethTransactions  = esTransactions.flatMap(EthTransaction.build(_, None))
-        _               <- transactionNeo.pushTransactions(ethTransactions)
-      } yield ethTransactions
+    private def transactions(walletId: EthAddressId): F[Set[EthTransaction]] =
+      esClient
+        .getTransactions(walletId, config.numBlocks)
+        .map(_.flatMap(EthTransaction.build(_, None)))
   }
 }
